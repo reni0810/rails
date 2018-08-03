e22ddd2f3b250023d3a407f00ef15959990a9e42
reni0810 <reni.dhingani@softices.com>
2018-08-03 18:29:01 +0530

    convert erb to haml formate

--- restaurant/config/environments/development.rb
+++ restaurant/config/environments/development.rb
@@ -5,8 +5,21 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  #config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.default_url_options =  { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'reni.dhingani@gmail.com',
+    password:             'aukr0810',
+    authentication:       :plain,
+    enable_starttls_auto: true
+}
   # Do not eager load code on boot.
   config.eager_load = false
   # Show full error reports.

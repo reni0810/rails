f6a2ce2f456bd9a9114cab779bcbb0b70f7fab58
reni0810 <reni.dhingani@softices.com>
2018-08-09 18:46:21 +0530

    Activity #4217 Runtime Exception 400 Error(change path)

--- restaurant/app/controllers/application_controller.rb
+++ restaurant/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def page_error
 		respond_to do |format|
-      format.html { render file: File.join(Rails.root, 'public', '404.html'), layout: 'layouts/application', status: 404 }
+      format.html { render file:'public/404.html', status: 404 }
       format.all  { render nothing: true, status: 404 }
     end	
   end

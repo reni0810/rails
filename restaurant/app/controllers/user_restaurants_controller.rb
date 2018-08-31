8ed5799afec143ee88951b945997791e61058a60
reni0810 <reni.dhingani@softices.com>
2018-08-31 16:50:32 +0530

    Activity #4263 Add search on My restaurant page.

--- restaurant/app/controllers/user_restaurants_controller.rb
+++ restaurant/app/controllers/user_restaurants_controller.rb
@@ -1,6 +1,11 @@
 class UserRestaurantsController < ApplicationController
   def index
-      @restaurants = Restaurant.where(user_id: current_user.id).order(:name).page(params[:page]).per(1)
+    search = {}
+    search[:user_id] = current_user.id
+    search[:name] = params[:search] if params[:search].present?
+    search[:"restaurants_cuisines.cuisine_id"] = params[:cuisine_id] if params[:cuisine_id].present?
+    search[:"restaurants_facilities.facility_id"] = params[:facility_id] if params[:facility_id].present?
+    search[:"unavailabities.date"] = params[:date] if params[:date].present?
+    @restaurants = Restaurant.left_outer_joins(:restaurants_cuisines,:restaurants_facilities,:unavailabities).where(search).order(:name).page(params[:page]).per(2)
   end
-  
 end
